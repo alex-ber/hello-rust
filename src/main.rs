9378db368ac9404dbe866d8e479a0e72b41d5fff
alexander_ber <alexander.berkovich@pursway.com>
2025-05-02 23:05:32 +0000

    some changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-use std::io;
+//use std::io;
 
 
 fn main() {
     println!("Hello from the Rust Dev Container!");
     // Add more code here
-    let mut buffer = String::new(); // Create a buffer
-    io::stdin().read_line(&mut buffer).unwrap(); // Wait for user input
+    //let mut buffer = String::new(); // Create a buffer
+    //io::stdin().read_line(&mut buffer).unwrap(); // Wait for user input
+    let liteal: &str = "Hello";
+    let result: String =  newstr(liteal);
+    print!("{:#?}", result);
+    println!();
+
+    let intarray: [i32;5] = [10,12,22, 34, 56];
+    let result:  Vec<i32> = newints(&intarray[..intarray.length-1]);
+    print!("{:#?}", result);
+}
+
+fn newstr(s: &str) -> String {
+    let mut result: String = String::from(s); // Create an owned, mutable String
+    result.push_str("a");           // Append the slice "&str"
+    result         
+}
+
+fn newints(intslice: &[i32]) -> Vec<i32> {
+    let mut vec = intslice.to_vec();
+    vec.push(42);
+    vec
+
 }
